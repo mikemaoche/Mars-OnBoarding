581dd40d23e645b6ad1b86488e74bcd4c9542094
worker session <maochemike@yahoo.fr>
2018-06-25 21:08:58 +1200

    REACT JS, REACT DOM, SEMANTIC UI INSTALLED

---
+++ Mars/Mars/scripts/React/webpack.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+    mode: 'production',
+    context: __dirname,
+    entry: "./Customers.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['babel-preset-env', 'babel-preset-react']
+                }
+            }
+        }]
+    }
+}
